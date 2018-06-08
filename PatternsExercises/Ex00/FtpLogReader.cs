8328b77a26977a78ee0ae668285b0da057803b03
Mariyaeremina <Mariyaeremina@outlook.com>
2018-06-08 23:31:59 +0500

    initial commit

---
+++ PatternsExercises/Ex00/FtpLogReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patterns.Ex00.ExternalLibs;
+
+namespace Patterns.Ex00
+{
+    class FtpLogReader : ILogReader
+    {
+        private string login;
+        private string password;
+
+        public FtpLogReader(string login, string password, string filePath)
+        {
+            this.login = login;
+            this.password = password;
+        }
+        public string ReadLogFile(string identificator)
+        {
+            return (new FtpClient().ReadFile(login, password, identificator));
+        }
+    }
+}
---
+++ PatternsExercises/Ex00/LogImporterClient.cs
@@ -0,0 +1,18 @@
+﻿namespace Patterns.Ex00
+{
+    public class LogImporterClient //класс клиента, импртирующего логи
+    {
+        /// <summary>
+        /// TODO: Изменения нужно вносить в этом методе
+        /// </summary>
+        public void DoMethod()
+        {
+            //LogImporter importer = new LogImporter(new FileLogReader());
+            //importer.ImportLogs("ftp://log.txt");
+
+            LogImporter importer = new LogImporter(new FtpLogReader("login", "password", "path"));
+            importer.ImportLogs("ftp://log.txt");
+        }
+
+    }
+}
